44f85e74fdfe6b7a41fa45952853ea3478255c0c
YURI <53067551+taro0koma@users.noreply.github.com>
2020-05-29 15:41:11 +0900

    readLine()で取り込まれた文字列数値を数値に戻すこと

---
+++ Math.PIを使って円の面積を求めるコード.js
@@ -0,0 +1,8 @@
+const PI = Math.PI;
+const r = +(readLine());
+
+// 円の面積　半径＊半径＊円周率
+console.log(PI * r * r);
+
+// 円の周囲　２＊半径＊円周率
+console.log(2 * PI * r);
--- 円周率を定義するコード.js
+++
@@ -1,9 +0,0 @@
-// Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
-    const PI = Math.PI;
-    const r = +(readLine());
-    
-    // Print the area of the circle:
-    console.log(PI * r * r);
-    
-    // Print the perimeter of the circle:
-    console.log(2 * PI * r);
