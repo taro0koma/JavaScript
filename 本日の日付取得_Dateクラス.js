50031d54ae9f4ceedb6354441801af546b71d542
YURI <53067551+taro0koma@users.noreply.github.com>
2020-05-29 11:14:16 +0900

    Dateクラスで本日の日付を取得する

--- 本日の日付取得_Dateクラス.js
+++ 本日の日付取得_Dateクラス.js
@@ -1,4 +1,10 @@
+// Dateクラスのインスタンスを生成する
 let today = new Date()
+
+// Dateクラスのメソッドをインスタンス内で使う
+// 注意！getMonth()は0始まりなので出力するときは+1を忘れない
+// 　　　ただし文字列なので＋１するときはparseInt()やNumber()、+()を行う
+// 曜日はgetDay() これは日曜０始まり["日","月","火","水","木","金","土"][0]
 
 console.log("今日は"+ ( parseInt( today.getMonth()) + 1) +"月" +
                     today.getDate() +"日" +
